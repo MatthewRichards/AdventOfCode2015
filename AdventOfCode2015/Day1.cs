c381fce5bfb2603eab54ffc9db74a9fabd94f8e1
Matthew Richards <matthew.richards@softwire.com>
2016-12-18 15:06:29 +0000

    Framework for running code without having to rename Main all the time

---
+++ AdventOfCode2015/Day1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2015
+{
+  class Day1
+  {
+    public void Run()
+    {
+      Console.WriteLine("test");
+      Console.ReadLine();
+    }
+  }
+}
--- AdventOfCode2015/Program.cs
+++ AdventOfCode2015/Program.cs
@@ -8,8 +8,12 @@ namespace AdventOfCode2015
 {
   class Program
   {
-    static void Main(string[] args)
+    static void Main()
     {
+      var latestSolution = typeof(Program).Assembly.GetTypes().Where(t => t.Name.StartsWith("Day")).OrderByDescending(t => int.Parse(t.Name.Substring(3))).First();
+
+      var instance = latestSolution.GetConstructor(new Type[0]).Invoke(new object[0]);
+      latestSolution.GetMethods().Single(m => m.Name == "Run").Invoke(instance, new object[0]);
     }
   }
 }
